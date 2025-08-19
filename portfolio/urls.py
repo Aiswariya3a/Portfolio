191b0b2703b99e1e103935e094b20098265da318
Aiswariya S <113989649+Aiswariya3a@users.noreply.github.com>
2025-08-19 21:28:15 +0530

    Added robots.txt to guide search crawlers

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,16 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# portfolio/urls.py
 
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static   
+from django.conf.urls.static import static  
+from django.views.generic.base import TemplateView 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("mysite.urls")),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
 
 if settings.DEBUG:
